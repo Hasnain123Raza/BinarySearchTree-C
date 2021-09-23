1828a8510cbcb6de6b1a4625111cd31df3d4e95d
Hasnain Raza <hasnain.mohsan@gmail.com>
2021-09-23 19:45:12 -0400

    Added 3 traversals

--- src/driver.c
+++ src/driver.c
@@ -5,23 +5,13 @@
 
 #define PREFIX_BUFFER_SIZE 128
 
-void printBinarySearchTree(BinarySearchTreeNode *binarySearchTree, char *prefix, int isLeft)
-{
-	if (binarySearchTree == NULL)
-		return;
+void printBinarySearchTree(BinarySearchTreeNode *binarySearchTree, char *prefix, int isLeft);
 
-	printf("%s", prefix);
-	printf(isLeft ? "├──" : "└──");
-	printf("%d", binarySearchTree->value);
-	printf("\n");
-	
-	char newPrefix[PREFIX_BUFFER_SIZE];
-	strcat(newPrefix, prefix);
-	strcat(newPrefix,  (isLeft ? "│   " : "    "));
+void printInOrder(BinarySearchTreeNode *binarySearchTree);
 
-	printBinarySearchTree(binarySearchTree->left, newPrefix, 1);
-	printBinarySearchTree(binarySearchTree->right, newPrefix, 0);
-}
+void printPreOrder(BinarySearchTreeNode *binarySearchTree);
+
+void printPostOrder(BinarySearchTreeNode *binarySearchTree);
 
 int main(void)
 {
@@ -43,11 +33,75 @@ int main(void)
 			insertValueBinarySearchTree(binarySearchTree, input);
 		
 		printf("\n");
+
 		printBinarySearchTree(binarySearchTree, "", 0);
+		printf("\n");
+
+		printf("InOrder Traversal: ");
+		printInOrder(binarySearchTree);
+		printf("\n");
+
+		printf("PreOrder Traversal: ");
+		printPreOrder(binarySearchTree);
+		printf("\n");
+
+		printf("PostOrder Traversal: ");
+		printPostOrder(binarySearchTree);
 		printf("\n");
 	}
 
 	printf("Goodbye!\n");
 
 	return 0;
+}
+
+void printBinarySearchTree(BinarySearchTreeNode *binarySearchTree, char *prefix, int isLeft)
+{
+	if (binarySearchTree == NULL)
+		return;
+
+	printf("%s", prefix);
+	printf(isLeft ? "├──" : "└──");
+	printf("%d", binarySearchTree->value);
+	printf("\n");
+	
+	char newPrefix[PREFIX_BUFFER_SIZE];
+	strcat(newPrefix, prefix);
+	strcat(newPrefix,  (isLeft ? "│   " : "    "));
+
+	printBinarySearchTree(binarySearchTree->left, newPrefix, 1);
+	printBinarySearchTree(binarySearchTree->right, newPrefix, 0);
+}
+
+void printInOrder(BinarySearchTreeNode *binarySearchTree)
+{
+	if (binarySearchTree->left)
+		printInOrder(binarySearchTree->left);
+
+	printf("%d, ", binarySearchTree->value);
+
+	if (binarySearchTree->right)
+		printInOrder(binarySearchTree->right);
+}
+
+void printPreOrder(BinarySearchTreeNode *binarySearchTree)
+{
+	printf("%d, ", binarySearchTree->value);
+
+	if (binarySearchTree->left)
+		printPreOrder(binarySearchTree->left);
+
+	if (binarySearchTree->right)
+		printPreOrder(binarySearchTree->right);
+}
+
+void printPostOrder(BinarySearchTreeNode *binarySearchTree)
+{
+	if (binarySearchTree->left)
+		printPostOrder(binarySearchTree->left);
+
+	if (binarySearchTree->right)
+		printPostOrder(binarySearchTree->right);
+
+	printf("%d, ", binarySearchTree->value);
 }
